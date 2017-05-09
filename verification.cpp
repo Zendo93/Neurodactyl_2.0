e9802cf211a11c412dacf0ad219b2bb46fac2861
Zendo <richarduzsak@gmail.com>
2017-05-09 17:10:40 +0200

    added functionality to verification page

--- verification.cpp
+++ verification.cpp
@@ -1,18 +1,53 @@
 #include "verification.h"
 #include "ui_verification.h"
+#include <QPushButton>
+#include <QFileDialog>
 
 Verification::Verification(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Verification)
 {
     ui->setupUi(this);
+    QPushButton *button = new QPushButton("Preprocessing");
     QLabel *label1 = new QLabel("<u>Match Score</u>");
     label1->setStyleSheet("font-size: 10pt;");
     QLabel *label2 = new QLabel("0,76 ...");
-    ui->gridLayout->setRowMinimumHeight(0,50);
-    ui->gridLayout->setRowMinimumHeight(1,0);
+    ui->gridLayout->setRowMinimumHeight(0,10);
+    ui->gridLayout->setRowMinimumHeight(1,50);
+    ui->gridLayout->setRowMinimumHeight(2,0);
     ui->gridLayout->addWidget(label1,0,0,0,0,Qt::AlignCenter);
-    ui->gridLayout->addWidget(label2,1,0,1,1,Qt::AlignCenter);
+    ui->gridLayout->addWidget(button,1,0,1,1,Qt::AlignCenter);
+    ui->gridLayout->addWidget(label2,2,0,1,1,Qt::AlignCenter);
+
+
+    connect(ui->comboBox_2,SIGNAL(currentIndexChanged(const QString&)),
+            this,SLOT(switchDevice1(const QString&)));
+    connect(ui->comboBox,SIGNAL(currentIndexChanged(const QString&)),
+            this,SLOT(switchDevice2(const QString&)));
+
+    if (isSenzorSelected(ui->comboBox))
+    {
+        image1 = executeTheScanner(scanner);
+        drawTheImageFromTheScanner(image1,ui->graphicsView);
+    }
+    else
+    {
+        QGraphicsScene* scene = new QGraphicsScene();
+        QString imageFile = readImageFromComputer();
+        ui->graphicsView->setScene(scene);
+    }
+
+    if (isSenzorSelected(ui->comboBox_2))
+    {
+        image2 = executeTheScanner(scanner);
+        drawTheImageFromTheScanner(image2,ui->graphicsView_2);
+    }
+    else
+    {
+        QGraphicsScene* scene = new QGraphicsScene();
+        QString imageFile = readImageFromComputer();
+        ui->graphicsView_2->setScene(scene);
+    }
     //layout->addWidget(checkBox3,2,0,2,2,Qt::AlignTop);
 }
 
@@ -20,3 +55,83 @@ Verification::~Verification()
 {
     delete ui;
 }
+
+bool Verification::isSenzorSelected(QComboBox *comboBox){
+    if (comboBox->itemText(ui->comboBox->currentIndex()) == "Senzor")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
+unsigned char *Verification::executeTheScanner(FingerPrintScanner *scanner){
+    unsigned char *buffer;
+    scanner = new FingerPrintScanner();
+    scanner->inicializeScanner();
+    scanner->runTheScanner();
+    scanner->saveCapturedFingerIntoBuffer();
+    buffer = scanner->getBuffer();
+    scanner->deinicializeScanner();
+    delete scanner;
+    return buffer;
+}
+
+void Verification::drawTheImageFromTheScanner(unsigned char *image,QGraphicsView *graphicsView){
+    QGraphicsScene* scene = new QGraphicsScene();
+    QImage Img(image, 500, 500, QImage::Format_Grayscale8);
+    QPixmap item = QPixmap::fromImage(Img);
+    scene->addPixmap(item);
+    graphicsView->setScene(scene);
+}
+
+void Verification::switchDevice1(QString name){
+    if (name == "Senzor")
+    {
+        unsigned char *buffer;
+        buffer = executeTheScanner(scanner);
+        drawTheImageFromTheScanner(buffer,ui->graphicsView_2);
+    }
+    else
+    {
+         QString imageFile = readImageFromComputer();
+         drawImageFromTheComputer(imageFile,ui->graphicsView_2);
+    }
+}
+
+
+void Verification::switchDevice2(QString name){
+    if (name == "Senzor")
+    {
+        unsigned char *buffer;
+        buffer = executeTheScanner(scanner);
+        drawTheImageFromTheScanner(buffer,ui->graphicsView);
+    }
+    else
+    {
+         QString imageFile = readImageFromComputer();
+         drawImageFromTheComputer(imageFile,ui->graphicsView);
+    }
+}
+
+QString Verification::readImageFromComputer(){
+    QString imageFile;
+    imageFile = QFileDialog::getOpenFileName(this,
+                                             tr("Open Image"), "",
+                                             tr("(*.tif);(*.jpg);(*.png);All Files (*)"));
+    return imageFile;
+}
+
+void Verification::drawImageFromTheComputer(QString imageFile,QGraphicsView *graphicsView){
+    QGraphicsScene* scene = new QGraphicsScene();
+    /*QImage image;
+    image.load(imageFile);*/
+    QPixmap image(imageFile);
+    scene->addPixmap(image);
+    graphicsView->setScene(scene);
+}
+
+
--- verification.h
+++ verification.h
@@ -2,6 +2,9 @@
 #define VERIFICATION_H
 
 #include <QWidget>
+#include <QGraphicsView>
+#include <QComboBox>
+#include "FingerPrintScanner/fingerprintscanner.h"
 
 namespace Ui {
 class Verification;
@@ -15,8 +18,20 @@ public:
     explicit Verification(QWidget *parent = 0);
     ~Verification();
 
+public slots:
+    void switchDevice1(QString name);
+    void switchDevice2(QString name);
+
 private:
     Ui::Verification *ui;
+    FingerPrintScanner *scanner;
+    unsigned char *image1;
+    unsigned char *image2;
+    bool isSenzorSelected(QComboBox *comboBox);
+    unsigned char *executeTheScanner(FingerPrintScanner *scanner);
+    void drawTheImageFromTheScanner(unsigned char *image, QGraphicsView *graphicsView);
+    void drawImageFromTheComputer(QString imageFile,QGraphicsView *graphicsView);
+    QString readImageFromComputer();
 };
 
 #endif // VERIFICATION_H

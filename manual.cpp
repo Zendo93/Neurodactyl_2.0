1fd8c38e4a82cb4dd203d9c61e37421af6e72273
Zendo <richarduzsak@gmail.com>
2017-05-10 16:14:37 +0200

    added senzor and file functionality to manual/preprocessing page

--- manual.cpp
+++ manual.cpp
@@ -1,5 +1,11 @@
 #include "manual.h"
 #include "ui_manual.h"
+#include <QFileDialog>
+#include "./FingerPrintScanner/fingerprintscanner.h"
+#include "./FingerPrintScanner/fxISOdll.h"
+#include "./FingerPrintScanner/fxISOenrdlg.h"
+#include "./FingerPrintScanner/fxISOAFIS.h"
+#include "./FingerPrintScanner/fxISO_Interchange.h"
 
 Manual::Manual(QWidget *parent) :
     QWidget(parent),
@@ -10,11 +16,36 @@ Manual::Manual(QWidget *parent) :
     createListForExtractionLevel2();
     createListForExtractionLevel1();
     createListForTrainingSettings();
+
+    scene = new QGraphicsScene();
+    connect(ui->comboBox,SIGNAL(activated(const QString&)),
+            this,SLOT(switchDevice(const QString&)));
+
+
+    connect(ui->checkBox, SIGNAL(clicked(bool)), this, SLOT(switchGPU(bool)));
+    connect(ui->checkBox_2, SIGNAL(clicked(bool)), this, SLOT(switchCPU(bool)));
+
+    if (isSenzorSelected(ui->comboBox))
+    {
+        image = executeTheScanner(scanner);
+        if (image != NULL)
+        {
+            drawTheImageFromTheScanner(image,ui->graphicsView,scene);
+        }
+    }
+    else
+    {
+        QString imageFile = readImageFromComputer();
+        ui->graphicsView->setScene(scene);
+    }
 }
 
 Manual::~Manual()
 {
     delete ui;
+    delete scanner;
+    delete image;
+    delete scene;
 }
 
 void Manual::createListForPreProcessing(){
@@ -67,4 +98,80 @@ void Manual::createListForTrainingSettings(){
     layout->addWidget(checkBox1,0,0,0,0,Qt::AlignTop);
     layout->addWidget(checkBox2,1,0,1,1,Qt::AlignTop);
     layout->addWidget(checkBox3,2,0,2,2,Qt::AlignTop);
+}
+
+bool Manual::isSenzorSelected(QComboBox *comboBox){
+    if (comboBox->itemText(ui->comboBox->currentIndex()) == "Senzor")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+unsigned char *Manual::executeTheScanner(FingerPrintScanner *scanner){
+    unsigned char *buffer;
+    scanner = new FingerPrintScanner();
+    scanner->inicializeScanner();
+    scanner->runTheScanner();
+    scanner->saveCapturedFingerIntoBuffer();
+    buffer = scanner->getBuffer();
+    scanner->deinicializeScanner();
+    delete scanner;
+    return buffer;
+}
+
+void Manual::drawTheImageFromTheScanner(unsigned char *image,QGraphicsView *graphicsView, QGraphicsScene* scene){
+    QImage Img(image, 500, 500, QImage::Format_Grayscale8);
+    QPixmap item = QPixmap::fromImage(Img);
+    scene->addPixmap(item);
+    graphicsView->setScene(scene);
+}
+
+void Manual::switchDevice(QString name){
+    if (name == "Senzor")
+    {
+        unsigned char *buffer;
+        buffer = executeTheScanner(scanner);
+        drawTheImageFromTheScanner(buffer,ui->graphicsView,scene);
+    }
+    else
+    {
+        QString imageFile = readImageFromComputer();
+        drawImageFromTheComputer(imageFile,ui->graphicsView,scene);
+    }
+}
+
+QString Manual::readImageFromComputer(){
+    QString imageFile;
+    imageFile = QFileDialog::getOpenFileName(this,
+                                             tr("Open Image"), "",
+                                             tr("(*.png *.jpg *.bmp *.tif);;All files (*.*)"));
+    return imageFile;
+}
+
+void Manual::drawImageFromTheComputer(QString imageFile,QGraphicsView *graphicsView,QGraphicsScene* scene){
+    QPixmap image(imageFile);
+    scene->addPixmap(image);
+    graphicsView->setScene(scene);
+}
+
+void Manual::switchGPU(bool checked){
+
+    if (checked)
+    {
+        ui->checkBox_2->setChecked(false);
+    }
+
+}
+
+void Manual::switchCPU(bool checked){
+
+    if (checked)
+    {
+        ui->checkBox->setChecked(false);
+    }
+
 }
--- manual.h
+++ manual.h
@@ -2,6 +2,9 @@
 #define MANUAL_H
 
 #include <QWidget>
+#include <QComboBox>
+#include <QGraphicsView>
+#include "FingerPrintScanner/fingerprintscanner.h"
 
 namespace Ui {
 class Manual;
@@ -15,8 +18,21 @@ public:
     explicit Manual(QWidget *parent = 0);
     ~Manual();
 
+public slots:
+    void switchDevice(QString name);
+    void switchGPU(bool checked);
+    void switchCPU(bool checked);
+
 private:
     Ui::Manual *ui;
+    FingerPrintScanner *scanner;
+    unsigned char *image;
+    QGraphicsScene* scene;
+    bool isSenzorSelected(QComboBox *comboBox);
+    unsigned char *executeTheScanner(FingerPrintScanner *scanner);
+    void drawTheImageFromTheScanner(unsigned char *image, QGraphicsView *graphicsView, QGraphicsScene* scene);
+    void drawImageFromTheComputer(QString imageFile,QGraphicsView *graphicsView, QGraphicsScene* scene);
+    QString readImageFromComputer();
     void createListForPreProcessing();
     void createListForExtractionLevel2();
     void createListForExtractionLevel1();
